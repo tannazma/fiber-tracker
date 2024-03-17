3e9918fc56d2e2c62a714881415db7fc8eb8d616
tannazma <97254921+tannazma@users.noreply.github.com>
2024-03-17 21:06:50 +0100

    removed boiler plate codes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <!-- <router-link to="/">Home</router-link> | -->
+    <!-- <router-link to="/about">About</router-link> -->
   </nav>
-  <router-view/>
+  <div>
+    <h1>Fiber Tracker</h1>
+  </div>
+  <router-view />
 </template>
 
 <style>
